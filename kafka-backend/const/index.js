8d49198bbc64ce2cd6482ac7d6588f26fcd2ba52
vamshireddy <32648706+vamshireddy08@users.noreply.github.com>
2018-12-23 11:25:10 -0800

    Update index.js

--- kafka-backend/const/index.js
+++ kafka-backend/const/index.js
@@ -1,8 +1,7 @@
 module.exports.KAFKA_URL = "localhost:2181";
-module.exports.MONGO_URL =
-  "mongodb://root:root123@ds155073.mlab.com:55073/linkedin";
+module.exports.MONGO_URL =" mlab url";
 module.exports.CONNECTION_LIMIT = 10000;
-module.exports.HOST = "linkedin.cx2nnmpqznns.us-east-1.rds.amazonaws.com";
+module.exports.HOST = "amazon rds";
 module.exports.USER = "linkedin_user";
 module.exports.PASSWORD = "linkedin_pass";
 module.exports.DATABASE = "linkedin_master";
